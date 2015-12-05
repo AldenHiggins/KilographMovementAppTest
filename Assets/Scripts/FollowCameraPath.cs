f142972f636748f2f5eb3bac7db457a58c4729f7
Alden Higgins <johnaldenhiggins@gmail.com>
2015-12-04 16:40:42 -0800

    First pass at camera path following

---
+++ Assets/Scripts/FollowCameraPath.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCameraPath : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public float cameraSpeed;
+    private int currentElementIndex;
+
+	void Start ()
+    {
+	    
+	}
+	
+	void Update ()
+    {
+        float distanceThisFrame = cameraSpeed * 10 * Time.deltaTime;
+        print("Distance: " + distanceThisFrame);
+        print("Delta time: " + Time.deltaTime);
+        // Get the next element to head towards
+        GameObject nextElement = transform.GetChild(currentElementIndex).gameObject;
+        // Find the direction to that next element
+        Vector3 directionToNextElement = nextElement.transform.position - mainCamera.transform.position;
+
+        // Check and see if we need to switch to the next element
+        if (directionToNextElement.magnitude < distanceThisFrame)
+        {
+            currentElementIndex++;
+
+            if (currentElementIndex == transform.childCount)
+            {
+                currentElementIndex = 0;
+            }
+
+            // Resample next element/direction
+            nextElement = transform.GetChild(currentElementIndex).gameObject;
+            directionToNextElement = nextElement.transform.position - mainCamera.transform.position;
+        }
+
+        // Normalize the direction vector
+        directionToNextElement.Normalize();
+
+        // Calculate the distance that you need to move
+        mainCamera.transform.position += directionToNextElement * distanceThisFrame;
+	}
+}
