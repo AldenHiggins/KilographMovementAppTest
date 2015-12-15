5173c9ebc5bc81f3ea9b08cfad227111c9fcef95
Alden Higgins <johnaldenhiggins@gmail.com>
2015-12-15 14:48:26 -0800

    Script to have world space UIs follow the camera

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour
+{
+    private Camera mainCamera;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mainCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.rotation = mainCamera.transform.rotation;
+	}
+}
