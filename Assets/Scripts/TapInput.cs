4606bbe1967c6d67cc88921afe71c04bef450152
Alden Higgins <johnaldenhiggins@gmail.com>
2015-12-18 12:03:13 -0800

    Added tap to go back in tour mode

--- Assets/Scripts/TapInput.cs
+++ Assets/Scripts/TapInput.cs
@@ -193,6 +193,8 @@ public class TapInput : MonoBehaviour
         }
     }
 
+    private bool secondTouch;
+
     void OnTouchEnded(int fingerId)
     {
         rightFingerId = -1;
@@ -208,8 +210,15 @@ public class TapInput : MonoBehaviour
             // If the user is in camera follow mode wait for tap then bring up the back button
             if (cameraFollowMode)
             {
-
-
+                if (secondTouch)
+                {
+                    generalBackButton.GetComponent<Image>().sprite = generalBackButton.GetComponent<ButtonSwitching>().onSprite;
+                    generalBackButton.GetComponent<Button>().interactable = false;
+                    exitWalkthrough();
+                    secondTouch = false;
+                }
+                secondTouch = true;
+                
             }
             else
             {
@@ -291,6 +300,7 @@ public class TapInput : MonoBehaviour
     /////////////////////////////////////////////////////////////////////////////////////////////
     void exitWalkthrough()
     {
+        secondTouch = false;
         cameraPath.SetActive(false);
 
         skyboxHotspot.SetActive(true);
@@ -316,7 +326,7 @@ public class TapInput : MonoBehaviour
     {
         cameraPath.SetActive(true);
 
-
+        secondTouch = false;
         generalBackButton.GetComponent<Image>().sprite = generalBackButton.GetComponent<ButtonSwitching>().offSprite;
         generalBackButton.GetComponent<Button>().interactable = true;
         generalBackButton.GetComponent<Button>().onClick.AddListener(exitWalkthrough);
