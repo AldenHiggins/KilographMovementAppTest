66549e1f33b991301cfefc39b67abfc06b8d5d9a
Alden Higgins <johnaldenhiggins@gmail.com>
2015-12-21 16:40:47 -0800

    Script to resize the splash screen for the ipad

---
+++ Assets/Scripts/ScreenResize.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenResize : MonoBehaviour
+{
+    public Vector3 ipadSize;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (Camera.main.aspect < 1.7)
+        {
+            transform.localScale = ipadSize;
+        }    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
